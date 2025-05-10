0d53754a52df8887bb1e120f93b2be489d175456
Jiyun Kwon <20087623@mail.wit.ie>
2025-05-10 14:43:10 +0100

    login home profile register pages

--- app/src/main/java/ie/setu/donationx/navigation/AppDestinations.kt
+++ app/src/main/java/ie/setu/donationx/navigation/AppDestinations.kt
@@ -2,9 +2,13 @@ package ie.setu.donationx.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.automirrored.filled.Login
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Details
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
@@ -42,8 +46,33 @@ object Details : AppDestination {
         navArgument(idArg) { type = NavType.IntType }
     )
 }
+object Home : AppDestination {
+    override val icon = Icons.Filled.Home
+    override val label = "Home"
+    override val route = "Home"
+}
 
-val bottomAppBarDestinations = listOf(Donate, Report, About)
-val allDestinations = listOf(Report, Donate, About, Details)
+
+object Profile : AppDestination {
+    override val icon = Icons.Default.Person
+    override val label = "Profile"
+    override val route = "Profile"
+}
+
+object Login : AppDestination {
+    override val icon = Icons.AutoMirrored.Filled.Login
+    override val label = "Login"
+    override val route = "Login"
+}
+
+object Register : AppDestination {
+    override val icon = Icons.Default.AccountCircle
+    override val label = "Register"
+    override val route = "Register"
+}
+val bottomAppBarDestinations = listOf(Donate, Report, About, Profile)
+val userSignedOutDestinations = listOf(Login, Register)
+val allDestinations = listOf(Report, Donate, About, Details, Home, Profile, Login, Register)
+
 
 
--- app/src/main/java/ie/setu/donationx/navigation/AppNavGraph.kt
+++ app/src/main/java/ie/setu/donationx/navigation/AppNavGraph.kt
@@ -3,7 +3,6 @@ package ie.setu.donationx.navigation
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -11,29 +10,40 @@ import androidx.navigation.compose.composable
 import ie.setu.donationx.ui.screens.about.AboutScreen
 import ie.setu.donationx.ui.screens.details.DetailsScreen
 import ie.setu.donationx.ui.screens.donate.ReviewScreen
+import ie.setu.donationx.ui.screens.home.HomeScreen
+import ie.setu.donationx.ui.screens.login.LoginScreen
+import ie.setu.donationx.ui.screens.profile.ProfileScreen
+import ie.setu.donationx.ui.screens.register.RegisterScreen
 import ie.setu.donationx.ui.screens.report.ListScreen
 
 @Composable
 fun NavHostProvider(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     navController: NavHostController,
+    startDestination: AppDestination,
     paddingValues: PaddingValues
 ) {
     NavHost(
         navController = navController,
-        startDestination = Report.route,
+        startDestination = startDestination.route,
         modifier = Modifier.padding(paddingValues = paddingValues)) {
 
         composable(route = Donate.route) {
             //call our 'review' Screen Here
             ReviewScreen(modifier = modifier)
         }
+
+        composable(route = Home.route) {
+            //call our 'Home' Screen Here
+            HomeScreen(modifier = modifier)
+        }
+
         composable(route = Report.route) {
             //call our 'list' Screen Here
             ListScreen(modifier = modifier,
                 onClickReviewDetails = {
-                    donationId : Int ->
-                    navController.navigateToDonationDetails(donationId)
+                        reviewId : Int ->
+                    navController.navigateToReviewDetails(reviewId)
                 },
             )
         }
@@ -41,6 +51,22 @@ fun NavHostProvider(
             //call our 'About' Screen Here
             AboutScreen(modifier = modifier)
         }
+        composable(route = Login.route) {
+            //call our 'Login' Screen Here
+            LoginScreen(
+                navController = navController,
+                onLogin = { navController.popBackStack() }
+            )
+        }
+
+        composable(route = Register.route) {
+            //call our 'Register' Screen Here
+            RegisterScreen(
+                navController = navController,
+                onRegister = { navController.popBackStack() }
+            )
+        }
+
 
         composable(
             route = Details.route,
@@ -52,10 +78,23 @@ fun NavHostProvider(
                 DetailsScreen()
             }
         }
+
+        composable(route = Profile.route) {
+            ProfileScreen(
+                onSignOut = {
+                    navController.popBackStack()
+                    navController.navigate(Login.route) {
+                        popUpTo(Home.route) { inclusive = true }
+                    }
+                }
+            )
+
+
+        }
     }
 }
 
-private fun NavHostController.navigateToDonationDetails(donationId: Int) {
-    this.navigate("details/$donationId")
+private fun NavHostController.navigateToReviewDetails(reviewId: Int) {
+    this.navigate("details/$reviewId")
 }
 
